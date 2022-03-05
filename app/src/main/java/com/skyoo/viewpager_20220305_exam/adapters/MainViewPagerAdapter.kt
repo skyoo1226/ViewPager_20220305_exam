17cd913889c4ce21ef73c17ff67e0da5c9dc3c64
skyoo1226 <you1226@naver.com>
2022-03-06 01:39:20 +0900

    상속의 필수 구현 함수 2개 오보라이딩

--- app/src/main/java/com/skyoo/viewpager_20220305_exam/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/skyoo/viewpager_20220305_exam/adapters/MainViewPagerAdapter.kt
@@ -1,7 +1,16 @@
 package com.skyoo.viewpager_20220305_exam.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+    }
 }
