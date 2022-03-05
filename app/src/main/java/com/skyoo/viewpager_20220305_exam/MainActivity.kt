cb0aca976b88cd8c50663cdc98fc7017fc122087
skyoo1226 <you1226@naver.com>
2022-03-06 02:00:28 +0900

    탭 레이아웃의 색상을 변경

--- app/src/main/java/com/skyoo/viewpager_20220305_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/viewpager_20220305_exam/MainActivity.kt
@@ -2,10 +2,40 @@ package com.skyoo.viewpager_20220305_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skyoo.viewpager_20220305_exam.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+        mAdapter = MainViewPagerAdapter( supportFragmentManager )
+        mainViewPager.adapter = mAdapter
+
+//        뷰페이저도 재사용성이 적용됨. 2~3장의 화면을 가지고, 프래그먼트를 상황에 따라 새로 만들거나
+//        함수 추가 호출 하는 등의 동작.
+
+//        3장 or 4장 등 원하는 페이지수 전체를 메모리에 유지하게 하는 명령어.
+        mainViewPager.offscreenPageLimit = 3
+
+
+//        탭레이아웃 / 뷰페이져 연결
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
+
+    }
+
 }
--- app/src/main/java/com/skyoo/viewpager_20220305_exam/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/skyoo/viewpager_20220305_exam/adapters/MainViewPagerAdapter.kt
@@ -3,14 +3,31 @@ package com.skyoo.viewpager_20220305_exam.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.skyoo.viewpager_20220305_exam.fragments.HelloFragment
+import com.skyoo.viewpager_20220305_exam.fragments.MyInfoFragment
+import com.skyoo.viewpager_20220305_exam.fragments.MynameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
-    override fun getCount(): Int {
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        return when(position) {
+            0 -> "이름"
+            1 -> "내 정보"
+            else -> "인사말"
+        }
 
     }
 
+    override fun getCount() = 3
+
     override fun getItem(position: Int): Fragment {
+
+        return when(position) {
+            0 -> MynameFragment()
+            1 -> MyInfoFragment()
+            else -> HelloFragment()
+        }
 
     }
 }
