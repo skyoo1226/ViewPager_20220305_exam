f1154314a3c5fcaf6fb62cdeb1c1b0dcaac655f7
skyoo1226 <you1226@naver.com>
2022-03-06 01:35:47 +0900

    뷰페리저 어댑터 클래스 추가 중  - 상속 받은 부모 생성자에 필요한 재료 맞춰 주기

---
+++ app/src/main/java/com/skyoo/viewpager_20220305_exam/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,7 @@
+package com.skyoo.viewpager_20220305_exam.adapters
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+}
